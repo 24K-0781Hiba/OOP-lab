5f3f322f2c1700a3332c66eaa8e9f9ceb1784902
24K-0781 <hibafaiq.9@gmail.com>
2025-02-04 18:40:53 +0500

    Update prob2.cpp

--- Lab2/prob2.cpp
+++ Lab2/prob2.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 using namespace std;  //idk what to do here so I just added array's size to each element
-void modify(int* arr, int size) {
-    for (int i = 0; i < size; i++) {
-        *(arr + i) += size; 
+void* modify(void* arr, int size) {
+    int* intArr = static_cast<int*>(arr);  
+    for (int i=0; i<size; i++) {
+        *(intArr +i) += size; 
     }
+    return arr; 
 }
-
 int main() {
-    int size, i;
+    int size;
     cout<<"Enter array size: ";
     cin >>size;
-    int* a= new int[size];
-    cout << "Enter " << size << " elements: ";
+    int* a =new int[size];  
+    cout <<"Enter "<<size<< " elements: ";
     for (int i = 0; i < size; i++) {
-        cin >>*(a+i);
+        cin >> *(a + i);
     }
-    modify(a, size);
-    cout << "Modified array: ";
-    for (i=0; i<size; i++) {
-        cout <<*(a+ i) << " ";
+    void* modifiedArr = modify(static_cast<void*>(a), size);  
+    cout << "Modified array is: ";
+    int* finalArr = static_cast<int*>(modifiedArr);
+    for (int i=0; i<size; i++) {
+        cout << *(finalArr + i) << " ";
     }
-    cout<< endl;
-    delete[] a;
+    cout <<endl;
+    delete[] a;  
     return 0;
 }
 
